e8871a5c4c447be43dff9e28c80ade66105d7799
Max Sun <max.sun@yale.edu>
2019-05-20 04:47:10 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,14 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  list = {}
+  data.each do |key, hash|
+    if key == :gender
+      hash.each do |gender, names|
+        names.each do |name|
+          data[name] = {gender: [gender.to_s]}
+        end
+      end
+    end
+  end
+  list
 end
